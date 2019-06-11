337e764ebdbdb893e51529a231bd4076c7d007b8
yangming <472102644@qq.com>
2019-06-11 13:46:57 +0800

    fix

--- src/components/List.vue
+++ src/components/List.vue
@@ -161,8 +161,8 @@ export default {
       // })
       const loading = this.$loading()
       const form = document.createElement('form')
-      form.action = 'http://localhost:3003/generate'
-      // form.action = 'http://iming.work:3003/generate'
+      // form.action = 'http://localhost:3003/generate'
+      form.action = 'http://iming.work:3003/generate'
       form.method = 'POST'
       const input = document.createElement('input')
       input.name = 'state'
